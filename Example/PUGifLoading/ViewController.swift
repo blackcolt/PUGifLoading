0f6e568a5f5cd9545c2700274e87387e2e47c7ce
Payal Umraliya <PayalUmraliya@users.noreply.github.com>
2018-11-13 11:01:13 +0530

    Update ViewController.swift

--- Example/PUGifLoading/ViewController.swift
+++ Example/PUGifLoading/ViewController.swift
@@ -1,23 +1,44 @@
 //
 //  ViewController.swift
-//  PUGifLoading
+//  LoaderSample
 //
-//  Created by PayalUmraliya on 11/13/2018.
-//  Copyright (c) 2018 PayalUmraliya. All rights reserved.
+//  Created by Payal on 13/11/18.
+//  Copyright © 2018 Payal. All rights reserved.
 //
 
 import UIKit
-
+import  PUGifLoading
 class ViewController: UIViewController {
-
+    //INIT LOADER CLASS OBJECT FIRST TO ACCESS ALL METHODS INSIDE IT
+    let loading = PUGIFLoading()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func btnShowClicked(_ sender: Any)
+    {
+                loading.hide()
+              //PU : - ADD TEXT YOU WANT TO SHOW AND PROVIDE GIF IMAGE NAME HERE AND IT MUST BE AVAILABLE IN PROJECT CODE
+                loading.show("Loading", gifimagename: "foodloader")
+                self.autohide()
+        
+    }
+    @IBAction func btnShowActivityClicked(_ sender: Any)
+    {
+                loading.hide()
+                loading.showWithActivityIndicator("Loading", activitycolor: UIColor.yellow, labelfontcolor:UIColor.yellow , labelfontsize: 16,activityStyle: UIActivityIndicatorViewStyle.whiteLarge)
+                self.autohide()
+    }
+    func autohide()
+    {
+                let when = DispatchTime.now() + 10
+                DispatchQueue.main.asyncAfter(deadline: when){
+                    //PU:- TO HIDE THE LOADER
+                    self.loading.hide()
+                }
     }
 
 }
